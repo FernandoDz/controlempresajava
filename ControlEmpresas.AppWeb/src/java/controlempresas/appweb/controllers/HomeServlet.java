5e8228c1d4bbfde81eb7578c9b5a00aca776d1f5
Fernando Diaz <dfer4738@gmail.com>
2023-08-21 23:55:16 +0200

    codificacion de  controladores en   app web

--- ControlEmpresas.AppWeb/src/java/controlempresas/appweb/controllers/HomeServlet.java
+++ ControlEmpresas.AppWeb/src/java/controlempresas/appweb/controllers/HomeServlet.java
@@ -1,4 +1,3 @@
-
 package controlempresas.appweb.controllers;
 
 import java.io.IOException;
@@ -9,8 +8,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @WebServlet(name = "HomeServlet", urlPatterns = {"/Home"})
 public class HomeServlet extends HttpServlet {
 
+    private void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGetRequestIndex(request, response);
+    }
 }
